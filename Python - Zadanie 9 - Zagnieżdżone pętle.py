1565ce4fcf9281b7f86a597579db39be24d8273d
Marek Grzesiak <marekgrzesiak.22@gmail.com>
2022-07-27 18:02:53 +0000

    Update Python - Zadanie 9 - Zagnieżdżone pętle.py - poprawka 1

--- Python - Zadanie 9 - Zagnieżdżone pętle.py
+++ Python - Zadanie 9 - Zagnieżdżone pętle.py
@@ -9,14 +9,17 @@
 # * $ *
 # * $
 # *
-for i in range(1):
-    for j in range(1):
-        print("*")
-        print("* $")
-        print("* $ *")
-        print("* $ * $")
-        print("* $ * $ *")
-    print("* $ * $ ")
-    print("* $ *")
-    print("* $")
-    print("*")
+def wzor():
+    for i in range(5):
+        for j in range(i + 1):
+            print('*' if j % 2 == 0 else '$', end=" ")
+        print()
+
+    for i in range(4):
+        for j in range(4 - i):
+            print('*' if j % 2 == 0 else '$', end=" ")
+        print()
+
+
+z = wzor()
+print(type(z))
