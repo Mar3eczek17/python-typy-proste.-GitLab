55603ce26e1841eb8744f371cfd9a67e1ca40058
Marek Grzesiak <marekgrzesiak.22@gmail.com>
2022-07-27 09:10:20 +0000

    Add new file Python - Zadanie 9 - Zagnieżdżone pętle.py

---
+++ Python - Zadanie 9 - Zagnieżdżone pętle.py
@@ -0,0 +1,22 @@
+# Zadanie 9.
+# Używajac zagnieżdżonych pętli, stwórz funkcję, która wydrukuje następujcy wzór:
+# *
+# * $
+# * $ *
+# * $ * $
+# * $ * $ *
+# * $ * $
+# * $ *
+# * $
+# *
+for i in range(1):
+    for j in range(1):
+        print("*")
+        print("* $")
+        print("* $ *")
+        print("* $ * $")
+        print("* $ * $ *")
+    print("* $ * $ ")
+    print("* $ *")
+    print("* $")
+    print("*")
