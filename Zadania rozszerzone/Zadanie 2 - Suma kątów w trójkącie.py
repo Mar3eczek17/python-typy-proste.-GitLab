a8f4112eaaf445f1f63c87d8ac330777e7d57fb5
Marek Grzesiak <marekgrzesiak.22@gmail.com>
2022-07-31 18:57:22 +0000

    Add new file Zadanie 2 - Suma kątów w trójkącie.py

---
+++ Zadania rozszerzone/Zadanie 2 - Suma kątów w trójkącie.py
@@ -0,0 +1,21 @@
+# Zadanie 2 - Suma kątów w trójkącie
+# Stwórz klasę Trójkat, w której konstruktor przyjmuje 3 argumenty,
+# reprezentujące kąty trójkąta. Klasa implementuje metodę sprawdz_katy,
+# która sprawdzi, czy suma kątow jest równa 180.
+class Trojkat(object):
+    def __init__(self, arg1, arg2, arg3):
+        self.arg1 = arg1
+        self.arg2 = arg2
+        self.arg3 = arg3
+
+    def sprawdz_katy(self):
+        if ((self.arg1 + self.arg2 + self.arg3 == 180) and self.arg1 != 0 and self.arg2
+                != 0 and self.arg3 != 0):
+            return True
+        else:
+            return False
+
+
+if __name__ == "__main__":
+    b = Trojkat(arg1=100, arg2=30, arg3=70)
+    print(b.sprawdz_katy().__str__())
