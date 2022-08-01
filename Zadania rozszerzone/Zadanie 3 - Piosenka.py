8ee4e789b1470f7cf3e54a4ab5c740eb2a6e6c5b
Marek Grzesiak <marekgrzesiak.22@gmail.com>
2022-08-01 14:01:11 +0000

    Update Zadanie 3 - Piosenka.py dodanie commit-a # Driver code

--- Zadania rozszerzone/Zadanie 3 - Piosenka.py
+++ Zadania rozszerzone/Zadanie 3 - Piosenka.py
@@ -11,18 +11,18 @@ class Piosenka(object):
             print(i)
 
 
-if __name__ == "__main__":
-    lista_wierszy = ['Dobrze się pan czuje?',
-                     'To świetnie,',
-                     'właśnie widzę - jasny wzrok, równy krok',
-                     'jak w marszu.',
-                     'A ja jestem, proszę pana, na zakręcie.',
-                     'Moje prawo to jest pańskie lewo.',
-                     'Pan widzi: krzesło, ławkę, stół,',
-                     'a ja - rozdarte drzewo.',
-                     'Bo ja jestem, proszę pana, na zakręcie.',
-                     'Ode mnie widać niebo przekrzywione.',
-                     'Pan dzieli każdą zimę, każdy świt na pół.',
-                     'Pan kocha swoja żonę.']
-    b = Piosenka(lista_wierszy)
-    b.spiewaj()
+# Driver code
+lista_wierszy = ['Dobrze się pan czuje?',
+                 'To świetnie,',
+                 'właśnie widzę - jasny wzrok, równy krok',
+                 'jak w marszu.',
+                 'A ja jestem, proszę pana, na zakręcie.',
+                 'Moje prawo to jest pańskie lewo.',
+                 'Pan widzi: krzesło, ławkę, stół,',
+                 'a ja - rozdarte drzewo.',
+                 'Bo ja jestem, proszę pana, na zakręcie.',
+                 'Ode mnie widać niebo przekrzywione.',
+                 'Pan dzieli każdą zimę, każdy świt na pół.',
+                 'Pan kocha swoja żonę.']
+b = Piosenka(lista_wierszy)
+b.spiewaj()
